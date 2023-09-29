98105373cb83cdd34b9a0603dbd0102cb552a657
fb07 <fbrangel27@gmail.com>
2023-09-29 00:28:33 -0500

    git sorted

--- neetleetcodeexamples/arrays/26.py
+++ neetleetcodeexamples/arrays/26.py
@@ -1,5 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums):
-        leftpointer = 1
-        for r in range(1, len(nums)):
-            
+        left = 1  # starting at index one since index 0 will be unique already
+        for i in range(
+            1, len(nums)
+        ):  # I have 1 starting because thats where we want to start in the 1st index position
+            if nums[i] != nums[i - 1]:
+                nums[left] = nums[i]
+                left += 1
+        return left
+
+    removeDuplicates([1, 1, 2])
